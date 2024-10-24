16f0c8b0a7b2bf1a8fc9e21d2b2d0f3f7894f69d
nflint-git <nmaf5352@gmail.com>
2024-10-24 23:27:55 +0100

    updated ch1 for polars refactor

--- cookbook/Chapter 1 - Reading from a CSV.py
+++ cookbook/Chapter 1 - Reading from a CSV.py
@@ -1,7 +1,7 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import polars as pl
 
 # %%
 # Reading data from a csv file
@@ -11,14 +11,17 @@ import matplotlib.pyplot as plt
 
 # This dataset is a list of how many people were on 7 different bike paths in Montreal, each day.
 
-broken_df = pd.read_csv("../data/bikes.csv", encoding="ISO-8859-1")
+#broken_df = pd.read_csv("C:/Users/nick/Projects/pandas_to_polars_cookbook/data/bikes.csv", encoding="ISO-8859-1")
 
 # TODO: please load the data with the Polars library (do not forget to import Polars at the top of the script) and call it pl_broken_df
-
+pl_broken_df = pl.read_csv("C:/Users/nick/Projects/pandas_to_polars_cookbook/data/bikes.csv", separator=";", encoding="utf8",try_parse_dates=True)
 # %%
 # Look at the first 3 rows
-broken_df[:3]
 
+#broken_df[:3]
+pl_broken_df = pl_broken_df.with_columns(
+    pl.col("Date").dt.strftime("%d-%m-%Y").alias("Date"))
+print(pl_broken_df.head(3))
 # TODO: do the same with your polars data frame, pl_broken_df
 
 # %%
@@ -30,6 +33,7 @@ broken_df[:3]
 # * Tell it that our dates have the day first instead of the month first
 # * Set the index to be the 'Date' column
 
+"""
 fixed_df = pd.read_csv(
     "../data/bikes.csv",
     sep=";",
@@ -38,7 +42,8 @@ fixed_df = pd.read_csv(
     dayfirst=True,
     index_col="Date",
 )
-fixed_df[:3]
+"""
+#fixed_df[:3]
 
 # TODO: do the same (or similar) with polars
 
@@ -48,22 +53,29 @@ fixed_df[:3]
 # When you read a CSV, you get a kind of object called a `DataFrame`, which is made up of rows and columns. You get columns out of a DataFrame the same way you get elements out of a dictionary.
 
 # Here's an example:
-fixed_df["Berri 1"]
-
+#fixed_df["Berri 1"]
+pl_fixed_df = pl_broken_df #since already fixed before
+print(pl_fixed_df.select("Berri 1"))
 # TODO: how would you do this with a Polars data frame?
 
 
 # %%
 # Plotting is quite easy in Pandas
-fixed_df["Berri 1"].plot()
+#fixed_df["Berri 1"].plot()
 
 # TODO: how would you do this with a Polars data frame?
 
-
+#plt.plot(pl_fixed_df.select("Berri 1"))
+#plt.show()
 # %%
 # We can also plot all the columns just as easily. We'll make it a little bigger, too.
 # You can see that it's more squished together, but all the bike paths behave basically the same -- if it's a bad day for cyclists, it's a bad day everywhere.
 
-fixed_df.plot(figsize=(15, 10))
+#fixed_df.plot(figsize=(15, 10))
+#plt.show()
 
 # TODO: how would you do this with a Polars data frame? With Polars data frames you might have to use the Seaborn library and it mmight not work out of the box as with pandas.
+pd_fixed_df = pl_fixed_df.to_pandas()
+plt.figure(figsize=(15, 10))
+plt.plot(pd_fixed_df.select_dtypes(include=['number']))
+plt.show()
